dc0883ee20ce793106ec641f1c7dd5c34b57cc6e
Imagine Engine <imagine2pi@gmail.com>
2020-10-11 18:41:22 +0000

    a lot

--- index.js
+++ index.js
@@ -7,26 +7,51 @@ var raw_data = fs.readFileSync('data.json')
 var file = JSON.parse(raw_data);
 
 var app = express()
-
+ 
 var server = app.listen(3000, listening)
+var players = 0;
+
+var game = {};
 
 function listening(){
   console.log('listening')
 }
 
 app.use(express.static('website'));
+app.use(express.json())
+//app.get('/room/:data', update)
+app.post('/room', roomState)
+app.post('/game', gameState)
 
-app.get('/room/:data', update)
+function transfer(request, response){
+  console.log(request.body)
+  response.send(players)
+}
 
-app.use(express.static('website'));
+function gameState(request, response){
+  //request.body[]
+  console.log(request.body)
+  response.send(JSON.stringify(game))
+}
 
+function roomState(request, response){
+  if (request.body.ip in game){
+
+  }
+  else{
+    game[request.body.ip] = {position:{x: 0, y: 0}}
+    players += 1
+  }
+  console.log(request.body.ip)
+  response.send(request.body.ip)
+}
 
 function update(request, response){
   var data = request.params
-  response.send('hey')
   file[data['data']] = 1
   fs.writeFile("data.json", JSON.stringify(file), a)
   console.log(data['data'])
+  response.send('hello there')
 }
 
 
---
+++ website/sketch.js
@@ -0,0 +1,30 @@
+var player;
+var ip;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  ellipse(0, 0, 25, 25);
+}
+
+
+async function start() {
+  await fetch('https://www.cloudflare.com/cdn-cgi/trace', {method: 'GET'}).then(response => response.text()).then(text => { ip = text.split('\n')[2].split('=')[1] })
+
+  await fetch('https://db.imagineengine.repl.co/room', { method: 'POST', }).then(response => response.text()).then(text => { player = text })
+  console.log(ip)
+  game(player)
+}
+
+function game(player) {
+  data = { 'player': player, motion: 'none' }
+  fetch('https://db.imagineengine.repl.co/game', { method: 'POST', body: JSON.stringify(data), headers: { 'Content-Type': 'application/json' } }).then(response => response.text()).then(text => console.log(text))
+
+}
+
+function end() { }
+
+start()
